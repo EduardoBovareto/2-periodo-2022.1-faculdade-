c29ce60e1803ea4f4f4f68755d4c2ae365cae0c5
Eduardo Bovareto <93995428+EduardoBovareto@users.noreply.github.com>
2022-01-21 20:15:04 -0300

    Add files via upload

---
+++ Desafio06.py
@@ -0,0 +1,8 @@
+numero = int(input('Digite um número para manipulação:'))
+Dobro = numero * 2
+tri = numero * 3
+raiz2 = numero * (1 / 2)
+print(f'O valor digitado foi:{numero};\nO dobro desse número é: {Dobro}; \no triplo desse número é: {tri};\n', end='')
+print(f'A Raiz quadrada desse número é: {raiz2:.2f}')
+
+# deu erro na raiz quadrada de 2, de 1.00 em vez de 1.41 ei acho!
